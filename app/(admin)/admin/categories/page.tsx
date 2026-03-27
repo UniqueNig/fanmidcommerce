55caed2f09f09fed844d5831cd02e1663ab01917
Emmanuel Faniyi <Faniyiemmanuel2018@gmail.com>
2026-03-27 14:36:34 +0100

    m
    	new file:   app/(admin)/admin/categories/page.tsx
    	new file:   app/(admin)/admin/customers/page.tsx
    	new file:   app/(admin)/admin/orders/page.tsx
    	new file:   app/(admin)/admin/products/[id]/edit/page.tsx
    	new file:   app/(admin)/admin/products/new/page.tsx
    	new file:   app/(admin)/admin/products/page.tsx
    	new file:   app/(admin)/admin/profile/page.tsx
    	new file:   app/(admin)/admin/settings/page.tsx

---
+++ app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+   <div>
+      <h1 className="text-2xl font-bold">Categories</h1>
+      <p>Manage categories here</p>
+    </div>
+  )
+}
+
+export default Categories
---
+++ app/(admin)/admin/profile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+     <div>
+      <h1 className="text-2xl font-bold">My Profile</h1>
+      <p>Update your personal details here.</p>
+    </div>
+  )
+}
+
+export default ProfilePage
